38983570303b9c6c5a47238310f0955baca9e418
Husseiny <husseiny.mohammed@gmail.com>
2019-10-15 22:46:06 +0200

    apply new arch. to products

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -6,7 +6,7 @@ import { ProductService } from '../product.service';
 import { GenericValidator } from '../../shared/generic-validator';
 import { NumberValidators } from '../../shared/number.validator';
 
-import * as fromProduct from '../state/product.reducer';
+import * as fromProduct from '../state';
 import * as productActions from '../state/product.actions';
 import { Store, select } from '@ngrx/store';
 
--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,7 +1,7 @@
-import * as fromRoot from '../../state/app.state';
 import { Product } from '../product';
-import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ProductActions, ProductActionTypes } from './product.actions';
+
+
 
 export interface ProductState {
   showProductCode: boolean;
@@ -16,50 +16,6 @@ const initialState = {
   products: [],
   error: ''
 };
-
-const getProductFeatureState = createFeatureSelector<ProductState>('products');
-
-export const getShowProductCode = createSelector(
-  getProductFeatureState,
-  (state) => state.showProductCode
-);
-
-export const getCurrentProductId = createSelector(
-  getProductFeatureState,
-  (state) => state.currentProductId
-);
-
-export const getCurrentProduct = createSelector(
-  getProductFeatureState,
-  getCurrentProductId,
-  (state, currentProductId) => {
-    if (currentProductId === 0) {
-      return {
-        id: 0,
-        productName: '',
-        productCode: 'New',
-        description: '',
-        starRating: 0
-      };
-    } else {
-      return currentProductId ? state.products.find((p) => p.id === currentProductId) : null;
-    }
-  }
-);
-
-export const getProducts = createSelector(
-  getProductFeatureState,
-  (state) => state.products
-);
-
-export const getError = createSelector(
-  getProductFeatureState,
-  (state) => state.error
-);
-
-export interface State extends fromRoot.State {
-  products: ProductState;
-}
 
 export function reducer(state = initialState, action: ProductActions) {
   switch (action.type) {
